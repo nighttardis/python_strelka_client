ddcb4c0761e83d054b282d4ccf8a09cd89e58991
nighttardis <henely@gmail.com>
2021-02-12 07:09:10 -0600

    setting up github actions

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ import setuptools
 
 setuptools.setup(
     name='python_strelka_client',
+    version="0.0.1",
     author='nighttardis',
     description='Python Client library for Strelka',
     license='Apache 2.0',
     packages=setuptools.find_packages(),
-    scripts=['bin/strelka-backend']
+    python_requires='>=3.7'
 )
